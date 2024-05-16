2303660b93fd9bd6cdffd374bf82bcc74c8db76b
Samuel Afonso Gomes <samuel.gomes.afonso@gmail.com>
2024-05-16 15:57:59 -0300

    some changes

--- lua/hackerman/init.lua
+++ lua/hackerman/init.lua
@@ -1,4 +1,4 @@
 require("hackerman.packer")
+-- require("hackerman.nvimtree")
 require("hackerman.remaps")
 require("hackerman.set")
-
--- lua/hackerman/packer.lua
+++ lua/hackerman/packer.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   use('nvim-treesitter/nvim-treesitter-context')
+  -- use('nvim-tree/nvim-tree.lua')
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
